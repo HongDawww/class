d9b3a35d97a68f99a1235b3a4e1b1c44c9135388
HongDawww <d939305@naver.com>
2023-11-24 15:10:11 +0900

    example page +

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+// 모든 페이지에서의 공통 설정을 여기서 진행
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} /> // Component : 페이지
 }
