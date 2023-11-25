659d5250e7d3eb9a34edd904508811c08d409ec0
HongDawww <d939305@naver.com>
2023-11-25 23:38:00 +0900

    게시글 번호로 이동

---
+++ pages/section05/static-routing-board-moved/1/index.js
@@ -0,0 +1,9 @@
+export default function SataticRoutingMovedPage(){
+	
+
+	return(
+		<div>
+			<div>1번 게시글 이동 완료</div>
+		</div>
+	)
+}
---
+++ pages/section05/static-routing-board-moved/3/index.js
@@ -0,0 +1,9 @@
+export default function SataticRoutingMovedPage(){
+	
+
+	return(
+		<div>
+			<div>3번 게시글 이동 완료</div>
+		</div>
+	)
+}
