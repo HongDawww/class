9932ee1ccb551ce45654482990a2574cea269639
HongDawww <d939305@naver.com>
2023-11-25 20:31:13 +0900

    ApolloProvider 설정 셋팅

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // 모든 페이지에서의 공통 설정을 여기서 진행
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
 export default function App({ Component, pageProps }) {
 
@@ -8,5 +8,9 @@ export default function App({ Component, pageProps }) {
     cache: new InMemoryCache() //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
   })
 
-  return <Component {...pageProps} /> // Component : 페이지
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} /> 
+    </ApolloProvider>
+  )
 }
