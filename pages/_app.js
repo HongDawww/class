53b989526af03e85b1e870afbcc49bda0843ee0c
HongDawww <d939305@naver.com>
2023-11-25 20:29:17 +0900

    apolloclient 설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 // 모든 페이지에서의 공통 설정을 여기서 진행
+import { ApolloClient, InMemoryCache } from "@apollo/client"
+
 export default function App({ Component, pageProps }) {
+
+  const client = new ApolloClient({
+    uri: "http://backend-example.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache() //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
+  })
+
   return <Component {...pageProps} /> // Component : 페이지
 }
