2bbd28e98967c543d95bc640f11774bdeea6891f
HongDawww <d939305@naver.com>
2023-11-25 23:24:12 +0900

    상세페이지 화면

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 export default function App({ Component, pageProps }) {
 
   const client = new ApolloClient({
-    uri: "http://backend-example.codebootcamp.co.kr/graphql",
+    uri: "https://backend-practice.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache() //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
   })
 
