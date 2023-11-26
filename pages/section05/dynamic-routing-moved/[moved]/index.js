8efd984ac8dacb2b4b45280213d74b5e0e8fc8eb
HongDawww <d939305@naver.com>
2023-11-27 08:14:30 +0900

    fetchBoard 오류 해결

---
+++ pages/section05/dynamic-routing-moved/[moved]/index.js
@@ -0,0 +1,37 @@
+import { useQuery, gql } from "@apollo/client"
+import { useRouter } from "next/router"
+
+const FETCH_BOARD = gql`
+	query fetchBoard($number: Int){
+		fetchBoard(number: $number){
+			number
+			writer
+			title
+			contents
+		}
+	}
+`
+
+export default function DynamicRoutingMovedPage(){
+	const router = useRouter();
+	// console.log(router);
+
+	const { data } = useQuery(FETCH_BOARD, {
+		variables: {
+
+			number: Number(router.query.moved)
+
+		}
+	})
+
+	// console.log(data)
+
+	return(
+		<div>
+			<div> {router.query.moved}번 게시글 이동 완료</div>
+			<div>작성자 : {data?.data.fetchBoard?.writer}</div>
+			<div>제목 : {data?.data.fetchBoard?.title}</div>
+			<div>내용 : {data?.data.fetchBoard?.contents}</div>
+		</div>
+	)
+}
