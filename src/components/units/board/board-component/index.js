2589b14c09d94133280bdb30fbc118660716a381
HongDawww <d939305@naver.com>
2023-11-27 19:38:50 +0900

    isEdit 삼항 연산자 활용

--- src/components/units/board/board-component/index.js
+++ src/components/units/board/board-component/index.js
@@ -9,7 +9,7 @@ export default function BoardComponent(props){
 			<h1>{props.isEdit ? "수정" : "등록"}페이지</h1>
 			제목 : <input type="text" /><br />
 			내용 : <input type="text" /><br />
-			<button>{props.name}하기</button>
+			<button>{props.isEdit ? "수정" : "등록"}하기</button>
 		</div>
 
 	)
