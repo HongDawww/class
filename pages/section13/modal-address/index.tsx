d7cb85ebd49b4c749b0488ef2b0fc4683492eae9
HongDawww <d939305@naver.com>
2023-11-29 14:00:08 +0900

    address modal

--- pages/section13/modal-address/index.tsx
+++ pages/section13/modal-address/index.tsx
@@ -20,19 +20,30 @@ export default function ModalAlertPage(): JSX.Element {
 
   const handleComplete = (data: Address): void => {
     console.log(data);
+    setIsopen(false);
   };
 
   return (
     <>
       <button onClick={showModal}>모달창 열기</button>
-      <Modal
-        title="모달제목"
+      {/* <Modal
+        title="모달 숨기기"
         open={isOpen}
         onOk={handleOk}
         onCancel={handleCancel}
       >
         <DaumPostcodeEmbed onComplete={handleComplete} />
-      </Modal>
+      </Modal> */}
+      {isOpen && (
+        <Modal
+          open={true}
+          title="모달 삭제 / 종료"
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <DaumPostcodeEmbed onComplete={handleComplete} />
+        </Modal>
+      )}
     </>
   );
 }
