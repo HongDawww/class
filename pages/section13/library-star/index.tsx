7b6748c44b59f8c1efe621d58a950c6e9ca2975a
HongDawww <d939305@naver.com>
2023-11-29 13:17:40 +0900

    star +

---
+++ pages/section13/library-star/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Rate } from "antd";
+
+// const desc = ["terrible", "bad", "normal", "good", "wonderful"];
+
+export default function App(): JSX.Element {
+  const [value, setValue] = useState(3);
+  // 1단계 방식
+  //   const onChangeStar = (value: number): void => {
+  //     setValue(value);
+  //   };
+
+  // 2단계 방식
+  //   const onChangeStar = (value: number): any => setValue(value);
+
+  return (
+    //<Rate onChange={onChangeStar} value={value} /> //- 1단계방식
+    // <Rate onChange={onChangeStar} value={value} /> - 2단계 방식
+    //  <Rate onChange={(value: number):any => setValue(value)} value={value} /> - 3단계 방식
+    <Rate onChange={setValue} value={value} /> // - 4단계 방식
+  );
+}
