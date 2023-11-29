1886d892fcb44a5ab849fe796c2a0acfe3d13260
HongDawww <d939305@naver.com>
2023-11-29 09:03:09 +0900

    eslint 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,23 @@
 // 모든 페이지에서의 공통 설정을 여기서 진행
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
-import { AppProps } from "next/app"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
 
-export default function App({ Component }: AppProps) {
-
+export default function App({ Component }: AppProps): JSX.Element {
   const client = new ApolloClient({
     // 포폴용 주소
-    
+
     // uri: "https://backend-practice.codebootcamp.co.kr/graphql",
     // 연습용 주소
     // uri: "http://backend-example.codebootcamp.co.kr/graphql",
     uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache() //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
-  })
+    cache: new InMemoryCache(), //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
+  });
 
   return (
-    <ApolloProvider client={client}>
-      <Component  /> 
-    </ApolloProvider>
-  )
+    <div>
+      <ApolloProvider client={client}>
+        <Component />
+      </ApolloProvider>
+    </div>
+  );
 }
