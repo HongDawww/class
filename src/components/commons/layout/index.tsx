1482f7a9bd889893dc8f01f3a34f2796aae1893c
HongDawww <d939305@naver.com>
2023-11-30 17:40:53 +0900

    특정 주소에서 숨기기

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,23 +1,36 @@
+import { useRouter } from "next/router";
 import LayoutBanner from "./banner";
 import LayoutFooter from "./footer";
 import LayoutHeader from "./header";
 import LayoutNavigation from "./navigation";
+
+const HIDDEN_HEADERS = ["/boards", "/itsload", "/slider", "/signup-state"];
 
 interface ILayoutProps {
   children: JSX.Element;
 }
 
 export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  console.log("--------------");
+  console.log(router.asPath);
+  console.log("--------------");
+
+  const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
   return (
     <>
-      <LayoutHeader />
-      <LayoutBanner />
-      <LayoutNavigation />
+      {!isHiddenHeader && <LayoutHeader />}
+      {!isHiddenHeader && <LayoutBanner />}
+      {!isHiddenHeader && <LayoutNavigation />}
       <div style={{ height: "500px", display: "flex" }}>
-        <div style={{ width: "30%", backgroundColor: "orange" }}>사이드바</div>
+        {!isHiddenHeader && (
+          <div style={{ width: "30%", backgroundColor: "orange" }}>
+            사이드바
+          </div>
+        )}
         <div style={{ width: "79%" }}>{props.children}</div>
       </div>
-      <LayoutFooter />
+      {!isHiddenHeader && <LayoutFooter />}
     </>
   );
 }
