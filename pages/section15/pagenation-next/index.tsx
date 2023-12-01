0f7b0b5f1a1ac698aef2dc6b32158acb3fc8141b
HongDawww <d939305@naver.com>
2023-12-01 10:26:11 +0900

    이전, 다음 페이지

--- pages/section15/pagenation-next/index.tsx
+++ pages/section15/pagenation-next/index.tsx
@@ -3,7 +3,7 @@ import type {
   IQuery,
   IQueryFetchBoardsArgs,
 } from "../../../src/commons/types/generated/types";
-import { MouseEvent } from "react";
+import { useState, type MouseEvent } from "react";
 const FETCH_BOARDS = gql`
   query fetchBoards($page: Int) {
     fetchBoards(page: $page) {
@@ -16,6 +16,7 @@ const FETCH_BOARDS = gql`
 `;
 
 export default function StaticRoutingMovedPage(): JSX.Element {
+  const [startPage, setStartPage] = useState(1);
   const { data, refetch } = useQuery<
     Pick<IQuery, "fetchBoards">,
     IQueryFetchBoardsArgs
@@ -27,6 +28,15 @@ export default function StaticRoutingMovedPage(): JSX.Element {
     void refetch({ page: Number(event.currentTarget.id) });
   };
 
+  const onClickPrevPage = (): void => {
+    setStartPage(startPage - 10);
+    void refetch({ page: startPage - 10 });
+  };
+
+  const onClickNextPage = (): void => {
+    setStartPage(startPage + 10);
+    void refetch({ page: startPage + 10 });
+  };
   return (
     <div>
       {data?.fetchBoards.map((el) => (
@@ -36,11 +46,18 @@ export default function StaticRoutingMovedPage(): JSX.Element {
         </div>
       ))}
 
+      <span onClick={onClickPrevPage}>이전</span>
       {new Array(10).fill(1).map((_, index) => (
-        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
-          {index + 1}
+        <span
+          key={index + startPage}
+          id={String(index + startPage)}
+          onClick={onClickPage}
+        >
+          {index + startPage}
         </span>
       ))}
+      <span onClick={onClickNextPage}>다음</span>
+
       {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, index) => (
         <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
           {index + 1}
