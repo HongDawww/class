98e1315202ae7f511456c24467e1e7ca0d0e302b
HongDawww <d939305@naver.com>
2023-12-04 12:34:22 +0900

    yarn

---
+++ CLASS/pages/section19/image-ref/index.tsx
@@ -0,0 +1,48 @@
+import { gql, useMutation } from "@apollo/client";
+import type { ChangeEvent } from "react";
+import { useState } from "react";
+import type {
+  IMutation,
+  IMutationUploadFileArgs,
+} from "../../../src/commons/types/generated/types";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file)
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [imageUrl, setImageUrl] = useState("");
+  const [uploadFile] = useMutation<
+    Pick<IMutation, "uploadFile">,
+    IMutationUploadFileArgs
+  >(UPLOAD_FILE);
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    const file = event.target.files?.[0]; // 배열로 들어오는 이유? multiple : 여러개 드래그 가능
+    console.log(file);
+
+    const result = await uploadFile({
+      variables: {
+        file,
+      },
+    });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+  const onClickImage = () => {};
+  return (
+    <>
+      <div
+        style={{ width: "50px", height: "50px", backgroundColor: "gray" }}
+        onClick={onClickImage}
+      >
+        이미지선택
+      </div>
+      <input style={{ display: "none" }} type="file" onChange={onChangeFile} />
+      <img src={`https://storage.googleapis.com/${imageUrl}`} />
+    </>
+  );
+}
