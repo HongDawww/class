3f8883b515a3f1cb612d0ef8c3934364f059e702
HongDawww <d939305@naver.com>
2023-12-05 17:14:13 +0900

    로그인, accesstoken

--- CLASS/src/components/commons/apollo/index.tsx
+++ CLASS/src/components/commons/apollo/index.tsx
@@ -5,23 +5,33 @@ import {
   ApolloLink,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../commons/stores";
+
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 
+const GLOBAL_STATE = new InMemoryCache();
+
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
+  const [accessToken] = useRecoilState(accessTokenState);
+
   const uploadLink = createUploadLink({
-    uri: "https://backend-practice.codebootcamp.co.kr/graphql",
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
+    headers: {
+      Authoriztion: `Bearer ${accessToken}`,
+    },
   });
   const client = new ApolloClient({
     // 포폴용 주소
 
-    uri: "https://backend-practice.codebootcamp.co.kr/graphql",
+    uri: "http://backend-practice.codebootcamp.co.kr/graphql",
     // 연습용 주소
     // uri: "http://backend-example.codebootcamp.co.kr/graphql",
     // uri: "http://practice.codebootcamp.co.kr/graphql",
     link: ApolloLink.from([uploadLink]),
-    cache: new InMemoryCache(), //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
+    cache: GLOBAL_STATE, //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시 저장
   });
 
   // prettier-ignore
