76a51ccc0b0450db17a830db4d4b9622a34f069b
HongDawww <d939305@naver.com>
2023-12-06 21:42:30 +0900

    react-hook-form

---
+++ CLASS/pages/section24/react-hook-form/after.tsx
@@ -0,0 +1,29 @@
+import { useForm } from "react-hook-form";
+import { wrapAsync } from "../../../src/commons/libraries/asyncFunc";
+
+interface IFormData {
+  writer: string;
+  title: string;
+  contents: string;
+  boardAddress: {
+    addressDetail: string;
+  };
+}
+
+export default function GraphqlMutationPage(): JSX.Element {
+  const { register, handleSubmit } = useForm<IFormData>();
+
+  const onClickSubmit = (data: IFormData): void => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={wrapAsync(handleSubmit(onClickSubmit))}>
+      작성자: <input type="text" {...register("writer")} />
+      제목: <input type="text" {...register("title")} />
+      내용: <input type="text" {...register("contents")} />
+      주소 : <input type="text" {...register("boardAddress.addressDetail")} />
+      <button>GRAPHQL-API 요청하기</button>
+    </form>
+  );
+}
