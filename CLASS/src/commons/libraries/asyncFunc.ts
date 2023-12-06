a782a810baa9982d5f152ad646ba812f27af1f02
HongDawww <d939305@naver.com>
2023-12-06 21:50:34 +0900

    wrapFormAsync

--- CLASS/src/commons/libraries/asyncFunc.ts
+++ CLASS/src/commons/libraries/asyncFunc.ts
@@ -1,7 +1,13 @@
 import { FormEvent } from "react";
 
+export const wrapFormAsync =
+  (asyncFunc: () => Promise<void>) => (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    void asyncFunc();
+  };
+
 export const wrapAsync =
   (asyncFunc: () => Promise<void>) => (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
     void asyncFunc();
   };
