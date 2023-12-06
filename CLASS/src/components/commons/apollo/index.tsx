29ce7d4640886797f69d8b978a9039da675953d6
HongDawww <d939305@naver.com>
2023-12-06 12:07:21 +0900

    프리렌더링

--- CLASS/src/components/commons/apollo/index.tsx
+++ CLASS/src/components/commons/apollo/index.tsx
@@ -8,9 +8,9 @@ import {
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../../../commons/stores";
 import { createUploadLink } from "apollo-upload-client";
-
+import { useEffect } from "react";
 interface IApolloSettingProps {
-  children: React.ReactNode;
+  children: JSX.Element;
 }
 
 const GLOBAL_STATE = new InMemoryCache();
@@ -18,10 +18,27 @@ const GLOBAL_STATE = new InMemoryCache();
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
 
-  const result = localStorage.getItem("accessToken");
+  // 1. 프리렌더링 - process.browser
+  // if (process.browser) {
+  //   alert("반가워요");
+  //   const result = localStorage.getItem("accessToken");
+  //   console.log(result);
+  //   setAccessToken(result ?? "");
+  // } else {
+  //   console.log("프론트서버");
+  // }
+  // 1. 프리렌더링 - typeof window
+  // if (typeof window !== "undefined") {
+  //   console.log("브라우저다");
+  // } else {
+  //   console.log("프론트 서버다");
+  // }
 
-  console.log(result);
-  setAccessToken(result ?? "");
+  // 3. 프리렌더링 무시 - useEffect
+  useEffect(() => {
+    const result = localStorage.getItem("accessToken");
+    setAccessToken(result ?? "");
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: "http://backend-practice.codebootcamp.co.kr/graphql",
