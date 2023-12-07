8afd1db36d576c4702b11e5a782f8e6ad3ac6964
HongDawww <d939305@naver.com>
2023-12-07 11:50:10 +0900

    custom hook

--- CLASS/src/components/commons/hooks/ustMoveToPage.tsx
+++ CLASS/src/components/commons/hooks/ustMoveToPage.tsx
@@ -13,7 +13,7 @@ export const useMoveToPage = (): IUseMoveToPageReturn => {
 
   const onClickMoveToPage = (path: string) => () => {
     setVisitedPage(path); // 로그인 페이지인 경우 조건 추가
-    // localStorage.setItem("visitedPage", path);
+    localStorage.setItem("visitedPage", path);
     void router.push(path);
   };
 
