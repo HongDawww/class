0e537018d5280a9106cfc1f35f6d933b72d00329
HongDawww <d939305@naver.com>
2023-12-07 13:11:29 +0900

    HOF

---
+++ CLASS/pages/section24/typescript-generic-hof/index.ts
@@ -0,0 +1,24 @@
+// 1. HOF - 일반 함수
+
+function first<T>(arg1: T) {
+  return function second<U>(arg2: U): [T, U] {
+    return [arg1, arg2];
+  };
+}
+
+const result1 = first("영희")(8);
+
+// 1-1. HOF 화살표 함수
+// prettier-ignore
+const first2 = <T>(arg1: T) => <U>(arg2: U): [T, U] => {
+    return [arg1, arg2];
+  };
+
+const result2 = first("영희")(8);
+
+// prettier-ignore
+const 로그인체크 = <C>(Component: C) => <P>(props: P): [C, P] => {
+    return [Component, props];
+  };
+
+const result3 = 로그인체크("영희")(8);
