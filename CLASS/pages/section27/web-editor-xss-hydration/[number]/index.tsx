ce4e9415b6c638fd789496117a998d75da481ad4
HongDawww <d939305@naver.com>
2023-12-11 14:10:28 +0900

    hydration

---
+++ CLASS/pages/section27/web-editor-xss-hydration/[number]/index.tsx
@@ -0,0 +1,42 @@
+import { useQuery, gql } from "@apollo/client";
+import DOMPurify from "dompurify";
+import { useRouter } from "next/router";
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function DynamicRoutingMovedPage(): JSX.Element {
+  const router = useRouter();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: {
+      boardId: router.query.number,
+    },
+  });
+
+  return (
+    <div>
+      <div style={{ color: "red" }}>작성자 : {data?.fetchBoard?.writer}</div>
+      <div style={{ color: "green" }}>제목 : {data?.fetchBoard?.title}</div>
+      {typeof window !== "undefined" ? (
+        <div
+          style={{ color: "blue" }}
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(data?.fetchBoard?.contents),
+          }}
+        />
+      ) : (
+        <div style={{ color: "blue" }} />
+      )}
+      <div style={{ color: "brown" }}>주소 : </div>
+    </div>
+  );
+}
